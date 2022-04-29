ba94f450f547c02e85e55661d9c2c47e2a8b312a
edAstra <edastracontact@gmail.com>
2022-04-29 11:21:47 -0400

    Layout changes to index.html

--- public/javascripts/plot.js
+++ public/javascripts/plot.js
@@ -1,1 +1,26 @@
-document.getElementById("scaled-frame").contentDocument.location.reload(true);
+function getInputValue(){
+  // Selecting the input element and get its value 
+  var inputVal = document.getElementById("myInput").value;
+  const url = '/read/'+inputVal;
+  fetch(url, {mode:'cors'})
+    .then(
+      response => response.json()
+	).then(
+	  json => plot(json) // Handle here
+	);
+
+  function plot(trace){
+    var data = [trace]
+    var layout = {
+	  margin: {
+        l: 0,
+        r: 0,
+	    b: 10,
+	    t: 10,
+	    pad: 4
+	  }
+    }
+  
+    Plotly.newPlot('myDiv', data, layout);
+  }
+}
